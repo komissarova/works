34bd2562e6b10db4dd7f26ca5f1f7cbf38dfa930
komissarova <elya_komissarova@mail.ru>
2020-02-05 22:31:05 +0300

    Добавила файл index.js

---
+++ mainBlogPage_ReactJS/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+import { Provider } from 'react-redux';
+import store from './store';
+
+import Layout from './app/layouts/Layout';
+import Main from './app/pages/Main';
+import Users from './app/pages/Users';
+import User from './app/pages/User';
+import PageNotFound from './app/pages/PageNotFound';
+
+ReactDOM.render(<Provider store={store}>
+	<Router history={browserHistory}>
+		<Route path="/" component={Layout}>
+			<IndexRoute component={Main} />
+			<Route path="users" component={Users} >
+				<Route path=":userId" component={User} />
+			</Route>
+			<Route path="*" component={PageNotFound} />
+		</Route>
+	</Router>
+	</Provider>,
+	document.getElementById('root'));
