4602c6208846a04c260a2b49a65669a1502a3dda
komissarova <elya_komissarova@mail.ru>
2020-02-05 22:42:43 +0300

    Добавила файл usersActions.js для действий React-приложения

---
+++ mainBlogPage_ReactJS/src/app/actions/usersActions.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+
+export function fetchUsers() {
+  
+  return function(dispatch) {
+    dispatch({type: 'FETCH_USERS'});
+    
+    axios.get('http://jsonplaceholder.typicode.com/users/')
+      .then((response) => {
+        dispatch({
+          type: 'FETCH_USERS_FULFILLED',
+          payload: response.data,
+        })
+      })
+      .catch((err) => {
+        dispatch({type: 'FETCH_USERS_REJECTED', payload: err})
+      })
+  }
+}
