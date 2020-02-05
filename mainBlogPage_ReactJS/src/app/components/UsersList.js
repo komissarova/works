5c3963f2105673efd4d6f4dc75fff373b28518df
komissarova <elya_komissarova@mail.ru>
2020-02-05 22:42:06 +0300

    Добавила файл UsersList.js компонента UsersList

---
+++ mainBlogPage_ReactJS/src/app/components/UsersList.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+import User from './User';
+
+export default class UsersList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: []
+    };
+  }
+  
+  render() {
+    if ( !this.props.users.length ) {
+      return null;
+    }
+    
+    const users = this.props.users.map((user) => {
+      return <User key={ user.id } { ...user } />
+    });
+    
+    return (
+      <div>
+        <h1>Пользователи</h1>
+        { users }
+      </div>
+    )
+  }
+}
+
